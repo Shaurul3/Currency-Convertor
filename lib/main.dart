d68f404382a486636e85585867d72182575fba31
Shaurul3 <94381820+Shaurul3@users.noreply.github.com>
2023-12-27 22:22:46 +0200

    analyze-option setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
@@ -31,7 +31,7 @@ class _HomePageState extends State<HomePage> {
     if (value.isEmpty) {
       return true;
     }
-    final numeric = num.tryParse(value);
+    final num? numeric = num.tryParse(value);
 
     return numeric != null;
   }
@@ -39,53 +39,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Currency Convertor'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 20,),
-          Image.network('https://play-lh.googleusercontent.com/pOiddrf8_IPWiUOyhj1cxbz2CzWejYdFjs8H5qGfIxnQs8YMNqZr4ScRevMhVnjS7Sg=w240-h480',),
-          TextField(
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Currency Convertor'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            Image.network(
+              'https://play-lh.googleusercontent.com/pOiddrf8_IPWiUOyhj1cxbz2CzWejYdFjs8H5qGfIxnQs8YMNqZr4ScRevMhVnjS7Sg=w240-h480',
+            ),
+            TextField(
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
               decoration: InputDecoration(
                 hintText: 'Enter the amount in EUR',
                 errorText: error,
               ),
-            onChanged: (String value){
-              setState(() {
-                text = value;
-                if (!_isNumeric(value)) {
-                  error = 'Please enter a valid number';
-                } else {
-                  error = null;
-                }
-              });
-            },
-          ),
-          TextButton(
-            onPressed: (){
-              setState(() {
+              onChanged: (String value) {
+                setState(() {
+                  text = value;
+                  if (!_isNumeric(value)) {
+                    error = 'Please enter a valid number';
+                  } else {
+                    error = null;
+                  }
+                });
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    final double? amount = double.tryParse(text);
 
-                double? amount = double.tryParse(text);
-
-
-                if (amount != null) {
-                  convertedResult = amount * 4.97;
-                }
-              });
-            },
-            child: const Text('Convert!')
-          ),
-          if (convertedResult != null)
-            Text(
-              '$convertedResult RON',
-              style: TextStyle(fontSize: 20),
-            )
-        ],
-      )
-    );
+                    if (amount != null) {
+                      convertedResult = amount * 4.97;
+                    }
+                  });
+                },
+                child: const Text('Convert!')),
+            if (convertedResult != null)
+              Text(
+                '$convertedResult RON',
+                style: const TextStyle(fontSize: 20),
+              )
+          ],
+        ));
   }
 }
